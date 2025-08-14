d0cd670c5026f92faa703a1ae8f25d1e05c100eb
Gustavo Santos <133147233+GusAtSantos@users.noreply.github.com>
2025-08-14 00:26:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import PortfolioSPA from "./pages/PortfolioSPA.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./assets/styles/global.css";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div className="app">
-      <PortfolioSPA />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
