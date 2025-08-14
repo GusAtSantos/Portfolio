9b1161a3a05d340b2b546aada3deaacd97a76ce6
Gustavo Santos <133147233+GusAtSantos@users.noreply.github.com>
2025-08-14 00:27:37 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,9 @@ import "../assets/styles/footer_css.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <p className="copyright">
-          © {new Date().getFullYear()} Gustavo Santos. Todos os direitos reservados.
-        </p>
-        <div className="social-wrapper">
-          <Social />
-        </div>
+      <div className="footer-content">
+        <p>© {new Date().getFullYear()} Gustavo Santos. Todos os direitos reservados.</p>
+        <Social />
       </div>
     </footer>
   );
