f5b5a908c9d16b7d863438b553eb720370ae2fa1
Gustavo Santos <133147233+GusAtSantos@users.noreply.github.com>
2025-08-13 23:25:50 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Social from "./Social.jsx";
-import "../assets/styles/footer_css.css"
+import "../assets/styles/footer_css.css";
 
 function Footer() {
-
     return (
-        <>
-            <footer className="footer">
-                <p>© 2025 Gustavo Santos. Todos os direitos reservados.</p>
-               <Social/>
-            </footer>
-        </>
-    )
-
+        <footer className="footer">
+            <div className="footer-content">
+                <p className="footer-copyright">© 2025 Gustavo Santos. Todos os direitos reservados.</p>
+                <Social />
+            </div>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
