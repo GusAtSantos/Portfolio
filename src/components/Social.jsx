f9f06108ea463cc6fa164bbfab7e99aecb78d1a0
Gustavo Santos <133147233+GusAtSantos@users.noreply.github.com>
2025-08-13 23:39:13 -0300

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,43 +1,43 @@
 import React from "react";
-import "../assets/styles/global.css";
 import github from "../assets/images/4202098_github_code_developer_logo_icon.svg";
 import linkedin from "../assets/images/4202085_linkedin_logo_social_social media_icon.svg";
 import insta from "../assets/images/1298747_instagram_brand_logo_social media_icon.svg";
+import "../assets/styles/social_css.css";
 
 function Social() {
-    return (
-        <div className="social-container">
-            <a 
-                href="https://github.com/GusAtSantos" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="social-link"
-            >
-                <img src={github} alt="GitHub" className="social-icon" />
-                <span>GitHub</span>
-            </a>
-            
-            <a 
-                href="https://www.linkedin.com/in/gustavo-santos-002415288/" 
-                target="_blank"
-                rel="noopener noreferrer"
-                className="social-link"
-            >
-                <img src={linkedin} alt="LinkedIn" className="social-icon" />
-                <span>LinkedIn</span>
-            </a>
-            
-            <a 
-                href="https://www.instagram.com/gus._.santos._/" 
-                target="_blank"
-                rel="noopener noreferrer"
-                className="social-link"
-            >
-                <img src={insta} alt="Instagram" className="social-icon" />
-                <span>Instagram</span>
-            </a>
-        </div>
-    );
+  return (
+    <div className="social-links">
+      <a 
+        href="https://github.com/GusAtSantos" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="social-link"
+        aria-label="GitHub"
+      >
+        <img src={github} alt="GitHub" className="social-icon" />
+      </a>
+      
+      <a 
+        href="https://www.linkedin.com/in/gustavo-santos-002415288/" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-link"
+        aria-label="LinkedIn"
+      >
+        <img src={linkedin} alt="LinkedIn" className="social-icon" />
+      </a>
+      
+      <a 
+        href="https://www.instagram.com/gus._.santos._/" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-link"
+        aria-label="Instagram"
+      >
+        <img src={insta} alt="Instagram" className="social-icon" />
+      </a>
+    </div>
+  );
 }
 
 export default Social;
